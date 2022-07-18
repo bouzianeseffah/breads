7b5eb97936caecc1fda0570dc6a5dc7543246a99
bouzianes <fcbdreams1991@gmail.com>
2022-07-17 23:34:28 -0400

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -14,7 +14,12 @@ breads.get('/', (req, res) => {
 breads.get('/new', (req, res) => {
   res.render('new')
 })
- 
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
@@ -47,6 +52,18 @@ breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
 })
+//update
+breads.put('/:arrayIndex', (req, res) => {
+  if (req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+//edit 
+
 
 
 
